42b93f86b0ea29c00a2c31d99ea59ade2bb31b95
Greg Neagle <gregneagle@mac.com>
2013-10-21 14:59:54 -0700

    Simplified options dictionary generation; added random desktop picture selection.

--- extras/03_set_desktop_picture.py
+++ extras/03_set_desktop_picture.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-'''Uses Cocoa classes via PyObjC to set the desktop picture on all screens.
+'''Uses Cocoa classes via PyObjC to set a random desktop picture on all screens.
 Tested on Mountain Lion.
 
 See:
@@ -10,23 +10,21 @@ https://developer.apple.com/library/mac/documentation/Cocoa/Reference/Foundation
 
 https://developer.apple.com/library/mac/documentation/cocoa/reference/applicationkit/classes/NSScreen_Class/Reference/Reference.html
 '''
+import glob
+import random
 
 from AppKit import NSWorkspace, NSScreen
-from AppKit import NSWorkspaceDesktopImageScalingKey
-from AppKit import NSImageScaleProportionallyUpOrDown
-from AppKit import NSWorkspaceDesktopImageAllowClippingKey
 from Foundation import NSURL
 
-picture_path = "/Library/Desktop Pictures/Beach.jpg"
+pictures_glob = glob.glob("/Library/Desktop Pictures/*.jpg")
+picture_path = random.choice(pictures_glob)
 
-# generate a fileURL
+# generate a fileURL for the desktop picture
 file_url = NSURL.fileURLWithPath_(picture_path)
 
 # make image options dictionary
-options = {
-    NSWorkspaceDesktopImageScalingKey: NSImageScaleProportionallyUpOrDown,
-    NSWorkspaceDesktopImageAllowClippingKey: True
-}
+# we just make an empty one because the defaults are fine
+options = {}
 
 # get shared workspace
 ws = NSWorkspace.sharedWorkspace()
